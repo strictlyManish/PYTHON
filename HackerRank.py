90898bb61b58f28cc208918f9d133ba62c435981
strictlyManish <rajzmanish94@gmail.com>
2025-11-20 00:08:07 +0530

    hands on practics

--- HackerRank.py
+++ HackerRank.py
@@ -1,4 +1,37 @@
-n = int(input('Enter a Nob : '))
+# n = int(input('Enter a Nob : '))
 
-for i in range(1,n+1):
-    print(i, end='')
+# for i in range(1,n+1):
+#     print(i, end='')
+
+
+
+
+
+# set = {9}
+
+# set.add(int(10.0))
+# print(set)
+
+
+
+X = int(input('X is : '))
+Y = int(input('Y is : '))
+choice = input(f'What do you want to do with these? X: {X} Y: {Y}  => ')
+
+result = 0
+
+if choice == '+':
+    result = X + Y
+elif choice == '-':
+    result = X - Y
+elif choice == '*':
+    result = X * Y
+else:
+    print("Invalid choice!")
+
+print('result:', result)
+
+
+
+
+
