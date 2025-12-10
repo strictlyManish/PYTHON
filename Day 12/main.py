96fd1b5f23d4ca87f1b9a3feacea3694224fa761
strictlyManish <rajzmanish94@gmail.com>
2025-12-10 09:33:00 +0530

    comeplited as homework task

--- Day 12/main.py
+++ Day 12/main.py
@@ -1,4 +1,31 @@
-file = open("code.txt","w")
-file.write(' manish Kumar G6F6F6F6F6FF')
+# with open("code.txt","a") as f:
+#     f.write('My Name is Manish Kumar in python how can i belive')
 
-file.close()
+
+# with open("code.txt","w") as f:
+#     f.write('My Name is Manish Kumar in python how can i belive')
+
+import shutil
+import os
+import datetime
+# shutil.copy("code.txt","textFile.txt") 
+
+# os.rename("new_demo.txt", "text_demo.txt")
+
+
+
+# with open("text_demo.txt",'a') as f:
+#     f.write(f"Saumya logged in at {datetime.datetime.now()}\n")
+
+
+# with open("text_demo.txt") as f:
+#     data = f.read()
+#     count = 0
+#     for i in range(len(data)):
+#         print( data[i],i)
+#         count +=1
+
+#     print(count)    
+
+
+
